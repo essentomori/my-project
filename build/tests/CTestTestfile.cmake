8445adb1b306957e266f404a88f35c96b866beb3
MK <m1ke2112koff@gmail.com>
2025-09-17 22:50:41 +0300

    Initial commit

---
+++ build/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/mk/my-project/rpn_calculator/tests
+# Build directory: /home/mk/my-project/rpn_calculator/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[rpn_tests]=] "/home/mk/my-project/rpn_calculator/build/tests/rpn_tests")
+set_tests_properties([=[rpn_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/mk/my-project/rpn_calculator/tests/CMakeLists.txt;19;add_test;/home/mk/my-project/rpn_calculator/tests/CMakeLists.txt;0;")
